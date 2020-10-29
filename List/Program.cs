735e26caf4f9ab9469527f59d3a72795facd38ca
markusanton <markusa070@gmail.com>
2020-10-29 11:26:48 +0200

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\data\test.txt";
+            List<string> dataForomFile = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in dataForomFile)
+            {
+                Console.WriteLine($"Data from: {line}");
+            }
+
+            dataForomFile.Add("grapes");
+            File.WriteAllLines(filePath, dataForomFile);
+
+
+        }
+    }
+}
---
+++ Wishlist/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace Wishlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\data\wishlist.txt";
+            List<string> dataForomFile = File.ReadAllLines(filePath).ToList();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter wish: ");
+                string userInput = Console.ReadLine();
+                dataForomFile.Add(userInput);
+                File.WriteAllLines(filePath, dataForomFile);
+            }
+        }
+    }
+}
